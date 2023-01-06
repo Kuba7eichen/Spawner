adfb15fcc27e2c55501643f0ce73aae69682c246
Kuba7eichen <kuba.siebeneichen@gmail.com>
2023-01-06 14:38:55 +0100

    Spawning limited to 10

--- Assets/Scripts/SpawnOnClick.cs
+++ Assets/Scripts/SpawnOnClick.cs
@@ -7,15 +7,25 @@ public class SpawnOnClick : MonoBehaviour
 {
     [SerializeField] private GameObject _objectToSpawn;
     [SerializeField] private float _spawningDistance;
+    private List<GameObject> _objects;
     // Update is called once per frame
+
+    private void Start()
+    {
+        _objects = new List<GameObject>();
+    }
     void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame) // returns true when the mouse left button is pressed
         {
-            Vector3 mousePos = Mouse.current.position.ReadValue();
-            mousePos.z = _spawningDistance;
-            Vector3 Worldpos = Camera.main.ScreenToWorldPoint(mousePos);
-            Instantiate(_objectToSpawn, Worldpos, Quaternion.identity);
+            if (_objects.Count < 10)
+            {
+                Vector3 mousePos = Mouse.current.position.ReadValue();
+                mousePos.z = _spawningDistance;
+                Vector3 Worldpos = Camera.main.ScreenToWorldPoint(mousePos);
+                GameObject spawned = Instantiate(_objectToSpawn, Worldpos, Quaternion.identity);
+                _objects.Add(spawned);
+            }
         }
     }
 }
