06736024b88a10bd5b99aaf2b66d2db3272022ae
Kuba7eichen <kuba.siebeneichen@gmail.com>
2023-01-06 14:40:07 +0100

    Limit can be changed from editor

--- Assets/Scripts/SpawnOnClick.cs
+++ Assets/Scripts/SpawnOnClick.cs
@@ -7,6 +7,7 @@ public class SpawnOnClick : MonoBehaviour
 {
     [SerializeField] private GameObject _objectToSpawn;
     [SerializeField] private float _spawningDistance;
+    [SerializeField] private int _maxSpawnedObjects = 10;
     private List<GameObject> _objects;
     // Update is called once per frame
 
@@ -18,7 +19,7 @@ public class SpawnOnClick : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame) // returns true when the mouse left button is pressed
         {
-            if (_objects.Count < 10)
+            if (_objects.Count < _maxSpawnedObjects)
             {
                 Vector3 mousePos = Mouse.current.position.ReadValue();
                 mousePos.z = _spawningDistance;
